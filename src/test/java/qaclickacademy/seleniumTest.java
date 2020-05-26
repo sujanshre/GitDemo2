460df5fa86f34053302732d1d878ebf376598af1
sujan <sujanshrestha98765@gmail.com>
2020-05-25 19:45:09 -0500

    first commit

---
+++ src/test/java/qaclickacademy/seleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+	
+		@Test
+		public void BrowserAutomation() {
+			System.out.println("BrowserAutomation");
+			
+		}
+		@Test
+	public void elementsUi() {
+		System.out.println("elementsUi");
+			
+		}
+
+}
+
